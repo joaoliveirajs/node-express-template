ff5476c4ddd997be628fa085f453f32811154ff7
João Oliveira <joaoliveirajs@gmail.com>
2020-03-24 14:26:09 -0300

    feat: Setup routes management.

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get("/", (req, res) => {
   return res.send(
-    "<h1>Template made by <a href='https://github.com/joaoliveirajs'>@joaoliveirajs<a/></h1>"
+    "<h2>Template made by <a href='https://github.com/joaoliveirajs'>@joaoliveirajs<a/></h2>"
   );
 });
 
